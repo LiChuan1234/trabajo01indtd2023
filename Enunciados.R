d75b28f0a7497ef21266aaee9635e1df56c7dca5
Ester Aguilar <you@example.com>
2023-10-21 11:12:28 +0200

    Enunciado primer ejercicio

---
+++ Enunciados.R
@@ -0,0 +1,13 @@
+##### EJERCICIO 1
+
+tabla1 = crea.tablaX(c(1, 4, 3, 5,
+                       2, 1, 1, 3,
+                       5, 5, 2, 1,
+                       1, 5, 2, 4),
+                     numalternativas = 4, numestados = 4)
+
+#     e1  e2  e3  e4
+# d1  1   4   3   5
+# d2  2   1   1   3
+# d3  5   5   2   1
+# d4  1   5   2   4    
