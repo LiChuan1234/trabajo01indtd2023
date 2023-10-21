ca007cea617eefa24267b637f592f68099d863d2
Ester Aguilar <you@example.com>
2023-10-21 14:12:09 +0200

    Enunciado del segundo problema añadido

--- Enunciados.R
+++ Enunciados.R
@@ -1,13 +1,45 @@
 ##### EJERCICIO 1
 
-tabla1 = crea.tablaX(c(1, 4, 3, 5,
-                       2, 1, 1, 3,
-                       5, 5, 2, 1,
-                       1, 5, 2, 4),
-                     numalternativas = 4, numestados = 4)
+# tabla1 = crea.tablaX(c(1, 4, 3, 5,
+#                        2, 1, 1, 3,
+#                        5, 5, 2, 1,
+#                        1, 5, 2, 4),
+#                      numalternativas = 4, numestados = 4)
 
 #     e1  e2  e3  e4
 # d1  1   4   3   5
 # d2  2   1   1   3
 # d3  5   5   2   1
 # d4  1   5   2   4    
+
+
+
+
+##### PROBLEMA 2
+
+# Ana ha obtenido una oferta de trabajo en una empresa extranjera, 
+# y le dan la opción de teletrabajo o mudarse al país y trabajar presencialmente.
+# Si trabaja online, la empresa le ofrece 1500 euros más horas extra, que suponen
+# un 5% del sueldo mínimo, siendo los gastos diarios viviendo en su país de 500 euros.
+# Si se muda y trabaja presencialmente, la empresa le permite realizar proyectos
+# que supondrían un 15% extra del sueldo mínimo y las horas extra son un 7% del sueldo mínimo,
+# pero el alojamiento y gastos
+# diarios le supondrían 700 euros.
+# La empresa está ahora mismo realizando un proyecto que debe 
+# estar acabado dentro de un año. Sin embargo, este proyecto está teniendo varios
+# problemas y la empresa no sabe si para la fecha límite van a estar resueltos.
+# En el caso de que salga todo bien, la empresa no tendría ningún problema futuro,
+# pero si no logran terminar el proyecto, se verían obligados a despedir a 
+# las personas trabajando presencialmente y reducir el extra de las horas
+# extra a un 4%.
+# En caso de que Ana sea despedida de esa empresa, sabe que puede 
+# volver al trabajo que tiene actualmente con un ingreso mensual de 1300 euros.
+# ¿Debería Ana trabajar online o mudarse?
+
+
+
+
+
+
+
+
